0328c400909de0710cf0047fc3581dcff99188bd
oxyfalcon <akashsinghcool19990@gmail.com>
2023-08-25 22:34:46 +0530

    test_commit_home_mac

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Application',
       theme: ThemeData(
         colorSchemeSeed: ColorMap.bluePrimary,
         useMaterial3: true,
